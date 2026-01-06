9b9ad88dc49b9462e1e2bbd12e4210fa16c11d72
Moirand <andreansyah1815@gmail.com>
2026-01-06 21:10:46 +0700

    develop category section

---
+++ app/(landing)/components/home/categories.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiArrowRight } from "react-icons/fi";
+
+const categories = [
+    { name: "Running", imgPath: "category-running.svg" },
+    { name: "Tennis", imgPath: "category-tennis.svg" },
+    { name: "Basketball", imgPath: "category-basketball.svg" },
+    { name: "Football", imgPath: "category-football.svg" },
+    { name: "Badminton", imgPath: "category-badminton.svg" },
+    { name: "Swimming", imgPath: "category-swimming.svg" },
+];
+
+export const CategoriesSection = () => {
+    return (
+        <section id="category-section" className="container mx-auto pb-20">
+            <div className="flex justify-between">
+                <h2 className="font-bold text-2xl">Browse By Categories</h2>
+                <Link href="#" className="flex gap-2 text-primary font-medium">
+                    <span className="self-center">See All Categories</span>
+                    <FiArrowRight className="self-center" />
+                </Link>
+            </div>
+            <div className="grid grid-cols-6 gap-12 mt-8">
+                {categories.map((category) => (
+                    <div key={category.name} className="flex justify-center rounded-lg bg-linear-to-r from-[#F1F1F1] to-[F7F7F7] w-full aspect-square">
+                        <div className="self-center">
+                            <Image
+                                src={`/images/categories/${category.imgPath}`}
+                                alt={category.name}
+                                width={86}
+                                height={86}
+                            />
+                            <div className="text-primary font-medium text-xl mt-2.5 text-center">{category.name}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
