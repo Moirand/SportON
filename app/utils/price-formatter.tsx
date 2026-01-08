c9dc6a4ea19983a5ad9ed6e49ce7eb110bc43cd4
Moirand <andreansyah1815@gmail.com>
2026-01-08 18:57:30 +0700

    Add product detail page with actions and price formatting

---
+++ app/utils/price-formatter.tsx
@@ -0,0 +1,9 @@
+export const priceFormatter = (price: number) => {
+    const newFormat = Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        maximumSignificantDigits: 3,
+    }).format(price);
+
+    return newFormat;
+};
