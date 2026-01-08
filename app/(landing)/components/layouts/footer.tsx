44f0c62993b6a15ee470861bfacd8bbc8d52de2a
Moirand <andreansyah1815@gmail.com>
2026-01-08 19:28:11 +0700

    develop about-us page

--- app/(landing)/components/layouts/footer.tsx
+++ app/(landing)/components/layouts/footer.tsx
@@ -3,39 +3,45 @@ import Link from "next/link";
 
 export const Footer = () => {
     return (
-        <footer className="bg-dark-alternate text-white mt-52">
+        <footer className="bg-dark-alternate  text-white ">
             <div className="container mx-auto flex justify-between pt-14 pb-24">
                 <div className="w-105">
                     <Image
                         src="/images/logo-white.svg"
-                        alt="Logo"
+                        alt="logo sporton footer"
                         width={187}
-                        height={44} />
-                    <p className="mt-8">Engineered for endurance and designed for speed.  Experience gear that moves as fast as you do.</p>
+                        height={44}
+                    />
+                    <p className="mt-8">
+                        Engineered for endurance and designed for speed. Experience gear
+                        that moves as fast as you do.
+                    </p>
                 </div>
                 <div className="w-105 grid grid-cols-2">
                     <div className="flex gap-7 flex-col">
                         <Link href="#">Home</Link>
                         <Link href="#">Categories</Link>
                         <Link href="#">Products</Link>
-                        <Link href="#">About Us</Link>
+                        <Link href="/about-us">About Us</Link>
                     </div>
                     <div className="flex gap-7 flex-col">
                         <Link href="#">Instagram</Link>
                         <Link href="#">Facebook</Link>
-                        <Link href="#">Tiktok</Link>
-                        <Link href="#">Youtube</Link>
+                        <Link href="#">TikTok</Link>
+                        <Link href="#">YouTube</Link>
                     </div>
                 </div>
             </div>
             <div className="border-t border-t-white/15">
                 <div className="container mx-auto py-6.5 flex justify-between">
-                    <p>SportsOn © 2025 All Rights Reserverd.</p>
+                    <div>SportsOn © 2025 All Rights Reserverd.</div>
+
                     <div className="grid grid-cols-2 w-105">
                         <Link href="#">Privacy Policy</Link>
-                        <Link href="#">Terms & Conditions</Link>
-                    </div></div>
+                        <Link href="#">Terms Conditions</Link>
+                    </div>
+                </div>
             </div>
         </footer>
     );
-}
+};
