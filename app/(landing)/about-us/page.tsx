a332e10f23c2f4c2452408dd008eef7c8fb2343a
Moirand <andreansyah1815@gmail.com>
2026-01-08 19:48:27 +0700

    fixing about-us

--- app/(landing)/about-us/page.tsx
+++ app/(landing)/about-us/page.tsx
@@ -1,7 +1,9 @@
-export const AboutUs = () => {
+const AboutUs = () => {
     return (
         <main>
             <h1 className="text-4xl font-bold text-primary">Halo ini About Us</h1>
         </main>
     );
 };
+
+export default AboutUs;
